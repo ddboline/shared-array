f95cb41629ea8b869fa027d9a7ae451a823ebcee
Mathieu Mirmont <mat@parad0x.org>
2015-08-23 13:12:15 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(name    = 'SharedArray',
-      version = '0.2',
+      version = '0.3',
 
       # Description
       description      = 'Share numpy arrays between processes',
