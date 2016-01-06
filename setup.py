9842dd6db015b245164e309b06e065a5a739258e
Mathieu Mirmont <mat@parad0x.org>
2016-01-06 22:19:54 +0100

    Celebrate the new year!

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # This file is part of SharedArray.
-# Copyright (C) 2014 Mathieu Mirmont <mat@parad0x.org>
+# Copyright (C) 2014-2016 Mathieu Mirmont <mat@parad0x.org>
 #
 # SharedArray is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- src/shared_array.h
+++ src/shared_array.h
@@ -1,6 +1,6 @@
 /* 
  * This file is part of SharedArray.
- * Copyright (C) 2014 Mathieu Mirmont <mat@parad0x.org>
+ * Copyright (C) 2014-2016 Mathieu Mirmont <mat@parad0x.org>
  * 
  * SharedArray is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
